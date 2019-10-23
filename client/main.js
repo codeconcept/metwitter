f24be3dae0aaf9ff32fbfa75cf8996c90b1297bd
codeconcept <contact@codeconcept.fr>
2019-10-23 20:27:21 +0200

    Iterating

--- client/main.js
+++ client/main.js
@@ -4,3 +4,21 @@ import "../node_modules/bootstrap/scss/bootstrap.scss"
 
 import './main.html';
 
+Template.body.helpers({
+  tweets: [
+    {
+      _id: 1,
+      author: 'Sam',
+      title: 'First tweet',
+      message: 'Can you see me?',
+      creationDate: new Date().toISOString()
+    },
+    {
+      _id: 2,
+      author: 'Bob',
+      title: 'Meteor',
+      message: 'Meteor is great :)',
+      creationDate: new Date().toISOString()
+    }
+  ]
+})
