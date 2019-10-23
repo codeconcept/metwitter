938231fb310d9f5247f004d526677f99376b8cb2
codeconcept <contact@codeconcept.fr>
2019-10-23 20:48:37 +0200

    MongoDB first insert

--- client/main.js
+++ client/main.js
@@ -3,22 +3,10 @@ import "../node_modules/bootstrap/scss/bootstrap.scss"
 // import 'bootstrap/dist/js/bootstrap.bundle';
 
 import './main.html';
+import { Tweets } from './../imports/api/tweets.js';
 
 Template.body.helpers({
-  tweets: [
-    {
-      _id: 1,
-      author: 'Sam',
-      title: 'First tweet',
-      message: 'Can you see me?',
-      creationDate: new Date().toISOString()
-    },
-    {
-      _id: 2,
-      author: 'Bob',
-      title: 'Meteor',
-      message: 'Meteor is great :)',
-      creationDate: new Date().toISOString()
-    }
-  ]
+  tweets() {
+    return Tweets.find({})
+  }
 })
